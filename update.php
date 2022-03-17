5f573bc4c8e03a0e1c65854490978afabfff8838
Louhela <niko.louhela@gmail.com>
2022-03-17 10:59:22 +0200

    first commit

---
+++ update.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+
+$input = json_decode(file_get_contents('php://input'));
+$id = filter_var($input->id,FILTER_SANITIZE_SPECIAL_CHARS);
+$description = filter_var($input->description,FILTER_SANITIZE_SPECIAL_CHARS);
+$amount = filter_var($input->amount,FILTER_SANITIZE_SPECIAL_CHARS);
+
+try{
+    $db = openDB();
+
+    $query = $db->prepare('update item set description=:description, amount=:amount where id=:id');
+    $query->bindValue('description', $description,PDO::PARAM_STR);
+    $query->bindValue(':amount',$amount,PDO::PARAM_INT);
+    $query->bindValue(':id', $id,PDO::PARAM_INT);
+    $query->execute();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $id,'description' => $description, 'amount' => $amount);
+    print json_encode($data);
+}   catch (PDOException $pdoex){
+    returnError($pdoex);
+}
